6301a5e1879f1178ff364b82da01b7514eecd649
Eric Neves <ericnevesrodrigues@gmail.com>
2021-06-21 19:01:44 -0400

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+    response.json({msg: 'Hello World'})
+})
+
+app.listen(3000, () => 
+    console.log('Server running on PORT 3000')
+)
