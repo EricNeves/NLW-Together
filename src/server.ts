156ac19b4fc5fb24b3880bcd03d1cba3deee9f9c
Eric Neves <ericnevesrodrigues@gmail.com>
2021-06-24 23:53:20 -0400

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import 'reflect-metadata'
 import 'express-async-errors'
 import { router } from './routes'
 import { config } from './config/settings'
+import cors from 'cors'
 
 import './database'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(router)
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
