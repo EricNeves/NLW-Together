dc879cdcc8e260362d8525be8d8fce8e43b6f4db
Eric Neves <ericnevesrodrigues@gmail.com>
2021-06-26 10:19:23 -0400

    Build app

---
+++ dist/middlewares/ensureAuthenticated.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ensureAuthenticated = void 0;
+var jsonwebtoken_1 = require("jsonwebtoken");
+function ensureAuthenticated(request, response, next) {
+    var authToken = request.headers.authorization;
+    if (!authToken)
+        return response.status(401).json({
+            msg: "Unauthorized"
+        });
+    var _a = authToken.split(' '), token = _a[1];
+    try {
+        var sub = jsonwebtoken_1.verify(token, "6fa18a0ac995377c6582912a7524018c").sub;
+        request.user_id = sub;
+        return next();
+    }
+    catch (err) {
+        return response.status(401).json({
+            error: err
+        });
+    }
+}
+exports.ensureAuthenticated = ensureAuthenticated;
