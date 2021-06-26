5b8f53ae68b19602d653b91a00dcd4d2bfb6efe2
Eric Neves <ericnevesrodrigues@gmail.com>
2021-06-25 21:39:41 -0400

    Update

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ app.use(router)
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
     if ( err instanceof Error ){
         response.status(400).json({
-            error: err.message
+            error: err.message 
         })
     }
     return response.status(500).json({
